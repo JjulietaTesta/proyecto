fab91bea3414186a060de1ad7e018149db070ec2
Julieta Testa <jjulietatesta@gmail.com>
2023-08-27 15:23:56 -0300

    auth con github

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,11 +1,69 @@
 import passport from "passport";
-import local from "passport-local";
+import GitHubStrategy from "passport-github2";
 import User from "../dao/models/user.js";
-import { createHash, isValidPassword } from "../utils.js";
+import * as dotenv from "dotenv";
+//import { createHash, isValidPassword } from "../utils.js";
+//import local from "passport-local";
 
-const LocalStrategy = local.Strategy
+dotenv.config();
+//const LocalStrategy = local.Strategy
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
+const GITHUB_CALLBACKURL = process.env.GITHUB_CALLBACKURL
+
+
 
 const initializePassport = ()=>{
+    passport.use(
+    'github', 
+    new GitHubStrategy(
+        {
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: GITHUB_CALLBACKURL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({email: profile?.emails[0]?.value})
+                if (!user) {
+                    const newUser = {
+                        first_name : profile.displayName.split(" ")[0],
+                        last_name : profile.displayName.split(" ")[1],
+                        email: profile?.emails[0]?.value,
+                        age: 26,
+                        password: "",
+                        } 
+                
+                let result = await User.create(newUser)
+                done (null, result)
+                }
+               else {
+                done (null, user)
+               }
+            }
+            catch (error) {
+              done (error, null)
+            }
+        }
+    ))
+
+
+    passport.serializeUser((user, done)=>{
+        done(null, user._id)
+    })
+
+    passport.deserializeUser(async(id, done)=>{
+        try {
+        let user = await User.findById(id)
+        done (null, user)
+        } catch (err) {
+            done (err,null)
+        }
+    })
+
+}
+    
+    /*const initializePassport = ()=>{
     passport.use(
     'register', 
     new LocalStrategy(
@@ -34,19 +92,11 @@ const initializePassport = ()=>{
                 return done ('Error al obtener usuario', error)
             }
         }
-    ))
-
-
-    passport.serializeUser((user, done)=>{
-        done(null, user._id)
-    })
-
-    passport.deserializeUser(async(id, done)=>{
-        let user = await User.findById(id)
-        done (null, user)
-    })
-
-
+    )) 
+    */
+    
+    
+   /*
     passport.use(
         "login",
         new LocalStrategy({
@@ -73,5 +123,17 @@ const initializePassport = ()=>{
         )
     )
 }
+passport.serializeUser((user, done)=>{
+        done(null, user._id)
+    })
 
+    passport.deserializeUser(async(id, done)=>{
+        let user = await User.findById(id)
+        done (null, user)
+    })
+
+
+
+
+*/
 export default initializePassport
--- src/router/session.router.js
+++ src/router/session.router.js
@@ -79,7 +79,20 @@ router.post("/signup", passport.authenticate('register',{
 )
 
 
+router.get ("/github", passport.authenticate("github", {scope: ["user:email"]}),
+  async(req, res) => {}
+)
 
+
+router.get(
+  "/githubcallback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  async (req, res) => {
+    req.session.user = req.user;
+    req.session.admin = true;
+    res.redirect("/");
+  }
+);
 
 
 export default router
